ea24a81f77ededc393804a8feef0963a189e3c8f
yeongdonge <parrineau@gmail.com>
2022-04-11 15:32:31 +0900

    Repository init

---
+++ src/main/java/rockplace/rdb2/Rdb2Application.java
@@ -0,0 +1,13 @@
+package rockplace.rdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rdb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/rockplace/rdb2/Rdb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package rockplace.rdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rdb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
